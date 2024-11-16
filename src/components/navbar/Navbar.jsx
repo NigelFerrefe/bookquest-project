44f9929708918a8f898415e72d5d4a9dbaffbeac
Nigel Ferreres <nferrefe@gmail.com>
2024-11-16 11:51:16 +0100

    navbar V.1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,39 +8,66 @@ import { useEffect, useState } from "react";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
 
   const handleMenu = (e) => {
     e.preventDefault();
     setIsOpen(!isOpen);
   };
 
+  const handleCloseMenu = () => {
+    setIsOpen(false); // Always close the menu
+  };
+
+  // Handle screen size changes
+  useEffect(() => {
+    // Function to resize
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+      setIsOpen(false); // Close menu on screen resize
+    };
+    // Add an event listener to the window to listen for resize events.
+    // Each time the window resizes, handleResize will be called.
+    window.addEventListener("resize", handleResize);
+
+    // Function to remove the resize event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="navbar-container">
       <div className="topnav">
-        <img id="logo-img" src={logo} alt="logo image" />
-        <button className="menu-btn" onClick={handleMenu}>
-          {isOpen ? (
-            <img width={"40rem"} src={closeBook} alt="closed btn" />
-          ) : (
-            <img width={"45rem"} src={openBook} alt="open btn" />
-          )}
-        </button>
+        <NavLink to="/" onClick={handleCloseMenu}>
+          <img id="logo-img" src={logo} alt="logo" />
+        </NavLink>
+        {isMobile && (
+          <button className="menu-btn" onClick={handleMenu}>
+            {isOpen ? (
+              <img width={"40rem"} src={openBook} alt="close menu" />
+            ) : (
+              <img width={"45rem"} src={closeBook} alt="open menu" />
+            )}
+          </button>
+        )}
       </div>
-      {!isOpen && (
+      {(!isMobile || isOpen) && (
         <ul className="navbar-links">
           <li>
-            <NavLink className="link" to="/">
+            <NavLink className="link" to="/" onClick={handleCloseMenu}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/about">
+            <NavLink className="link" to="/about" onClick={handleCloseMenu}>
               About
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/profile">
-              <img
+            <NavLink className="link" to="/profile" onClick={handleCloseMenu}>
+              <img 
+                id="profile-icon"
                 width={"35rem"}
                 src={profileIcon}
                 alt="profile user button"

ca3000f478f028e897ea7cb29a812c9404711c18
Nigel Ferreres <nferrefe@gmail.com>
2024-11-16 13:07:13 +0100

    notfound page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,30 @@
+import notFoundImg from "../assets/not-found-book.png";
+import "./NotFound.css";
+
 function NotFoundPage() {
-    return (
-        <h1>Error</h1>
-    )
+  return (
+    <>
+      <div className="container-error">
+        <div className="container-text">
+          <aside className="quote">
+            <blockquote>“Not all those who wander are lost.”</blockquote>
+            <p>— J.R.R. Tolkien</p>
+          </aside>
+          <div>
+            <img src={notFoundImg} alt="error img" id="not-found-img" />
+            <h3>Error 404</h3>
+          </div>
+          <p>
+            While it seems like you have stumbled upon a page that is no longer
+            here, your journey is far from over.
+          </p>
+          <p>The next great discovery awaits.</p>
+          <p>Let&apos;s find it.</p>
+        </div>
+        <button className="not-found-btn">Embark on a new adventure</button>
+      </div>
+    </>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;

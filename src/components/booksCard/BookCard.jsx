a529bef282c297d3d1d03b4e6d12922217bd8e37
Peedee01 <piethein@schouten.nl>
2024-11-19 23:26:52 +0100

    profile-page

--- src/components/booksCard/BookCard.jsx
+++ src/components/booksCard/BookCard.jsx
@@ -1,6 +1,9 @@
+import './BookCard.css'
+
+
 function BookCard({ book }) {
   return (
-    <div>
+    <div className='book-card'>
       <img width={200} src={book.image} alt={book.title} />
       <p>{book.title}</p>
       <p>{book.author}</p>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,63 @@
+import supabase from "../supabase/config";
+import { useState, useEffect } from "react";
+import BookCard from "../components/booksCard/BookCard";
+import { Link } from "react-router-dom";
+import "./ProfilePage.css";
+import Profile from "../assets/Profile-picture.png";
+
 function ProfilePage() {
-    return (
-        <h1>Profile</h1>
-    )
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  async function getFilteredBooks() {
+    try {
+      const response = await supabase
+        .from("books")
+        .select("*")
+        .eq("isBought", true)
+        .order("id", { ascending: false });
+
+      setFilteredBooks(response.data);
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+
+  useEffect(() => {
+    getFilteredBooks();
+  });
+
+  return (
+    <section>
+      <div className="yourprofile">
+        <div className="profile-info">
+          <h2>Your Profile</h2>
+          <img width={150} src={Profile} alt="profile-picture" />
+          <p>Name: </p>
+          <p>Favorite author: </p>
+          <p>Reading-Goals: </p>
+          <p>BookLinks: </p>
+        </div>
+        <div className="book-statistics">
+          <p>Books: {`${filteredBooks.length}`} </p>
+        </div>
+        <div className="genres">
+          <p>Genres: </p>
+        </div>
+      </div>
+    <div className="book-titles">
+      <h3>Your Books</h3>
+    </div>
+      <div className="books-container">
+        {filteredBooks.map((filteredBook) => (
+          <div className="personal-book">
+            <Link to={`/details/${filteredBook.id}`}>
+              <BookCard book={filteredBook} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;

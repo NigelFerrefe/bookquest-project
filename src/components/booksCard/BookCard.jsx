9b1dcfaf8b5c87e04d68f7c8d4978dcac083e1fd
Peedee01 <piethein@schouten.nl>
2024-11-20 16:03:55 +0100

    profile-page-styling

--- src/components/booksCard/BookCard.jsx
+++ src/components/booksCard/BookCard.jsx
@@ -4,8 +4,8 @@ import './BookCard.css'
 function BookCard({ book }) {
   return (
     <div className='book-card'>
+      <h4>{book.title}</h4>
       <img width={200} src={book.image} alt={book.title} />
-      <p>{book.title}</p>
       <p>{book.author}</p>
     </div>
   );

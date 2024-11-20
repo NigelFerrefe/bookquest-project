dc3bbadbc725600a33dacf106db5576226753f0e
Nigel Ferreres <nferrefe@gmail.com>
2024-11-20 17:36:58 +0100

    favorutei books

--- src/components/AllBooks/BookList.jsx
+++ src/components/AllBooks/BookList.jsx
@@ -6,32 +6,31 @@ import { Link, useSearchParams } from "react-router-dom";
 import SearchBar from "../Searchbar/SearchBar.jsx";
 import "./BookList.css";
 import GenreFilter from "../GenreFilter/GenreFilter.jsx";
+import unlikeIcon from "../../assets/heart-unlike.png";
+import likeIcon from "../../assets/heart-like.png";
 
 function AllBooks() {
   const [allBooks, setAllBooks] = useState([]);
   const [filteredBooks, setFilteredBooks] = useState([]);
   const [currentSlideAllBooks, setCurrentSlideAllBooks] = useState(0);
   const [currentSlideFilteredBooks, setCurrentSlideFilteredBooks] = useState(0);
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isLike, setIsLike] = useState(false);
 
   const booksPerSlidePhone = 1; // Number of books on phones screen
 
   async function getAllBooks() {
     const queryname = searchParams.get("title");
 
-    console.log("this is the query name: " , queryname)
+    console.log("this is the query name: ", queryname);
     let response;
 
-
     try {
-
       if (queryname) {
         response = await supabase
-        .from("books")
-        .select("*")
-        .ilike("title", `%${queryname}`)
-
-
+          .from("books")
+          .select("*")
+          .ilike("title", `%${queryname}`);
       } else {
         response = await supabase
           .from("books")
@@ -40,8 +39,8 @@ function AllBooks() {
           .order("id", { ascending: false });
       }
 
-      if (response.data.length === 0){
-        alert("No Results Found")
+      if (response.data.length === 0) {
+        alert("No Results Found");
       } else {
         setAllBooks(response.data);
       }
@@ -70,22 +69,37 @@ function AllBooks() {
   }, [searchParams]);
 
   const totalSlidesAllBooks = Math.ceil(allBooks.length / booksPerSlidePhone);
-  const totalSlidesFilteredBooks = Math.ceil(filteredBooks.length / booksPerSlidePhone);
+  const totalSlidesFilteredBooks = Math.ceil(
+    filteredBooks.length / booksPerSlidePhone
+  );
 
   const handleNextAllBooks = () => {
     setCurrentSlideAllBooks((prev) => (prev + 1) % totalSlidesAllBooks);
   };
 
   const handlePrevAllBooks = () => {
-    setCurrentSlideAllBooks((prev) => (prev === 0 ? totalSlidesAllBooks - 1 : prev - 1));
+    setCurrentSlideAllBooks((prev) =>
+      prev === 0 ? totalSlidesAllBooks - 1 : prev - 1
+    );
   };
 
   const handleNextFilteredBooks = () => {
-    setCurrentSlideFilteredBooks((prev) => (prev + 1) % totalSlidesFilteredBooks);
+    setCurrentSlideFilteredBooks(
+      (prev) => (prev + 1) % totalSlidesFilteredBooks
+    );
   };
 
   const handlePrevFilteredBooks = () => {
-    setCurrentSlideFilteredBooks((prev) => (prev === 0 ? totalSlidesFilteredBooks - 1 : prev - 1));
+    setCurrentSlideFilteredBooks((prev) =>
+      prev === 0 ? totalSlidesFilteredBooks - 1 : prev - 1
+    );
+  };
+
+  console.log("The favourtie book", allBooks.id);
+  
+  const handleLike = (id) => {
+    setIsLike((prev) => ({ ...prev, [id]: !prev[id] }));
+    console.log("this book is favourite now");
   };
 
   return (
@@ -131,36 +145,46 @@ function AllBooks() {
       <div className="all-books">
         <h2>Your Books</h2>
       </div>
-        <div className="carousel-container">
-          <button
-            onClick={handlePrevFilteredBooks}
-            className="carousel-button prev-button"
-          >
-            ‹
-          </button>
-          <div className="carousel-track-container">
-            <ul className="carousel-track">
-              {filteredBooks
-                .slice(
-                  currentSlideFilteredBooks * booksPerSlidePhone,
-                  (currentSlideFilteredBooks + 4) * booksPerSlidePhone
-                )
-                .map((filteredBook) => (
-                  <li key={filteredBook.id} className="carousel-slide">
-                    <Link to={`/details/${filteredBook.id}`}>
-                      <BookCard book={filteredBook} />
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <button
-            onClick={handleNextFilteredBooks}
-            className="carousel-button next-button"
-          >
-            ›
-          </button>
+      <div className="carousel-container">
+        <button
+          onClick={handlePrevFilteredBooks}
+          className="carousel-button prev-button"
+        >
+          ‹
+        </button>
+        <div className="carousel-track-container">
+          <ul className="carousel-track">
+            {filteredBooks
+              .slice(
+                currentSlideFilteredBooks * booksPerSlidePhone,
+                (currentSlideFilteredBooks + 4) * booksPerSlidePhone
+              )
+              .map((filteredBook) => (
+                <li key={filteredBook.id} className="carousel-slide">
+                  <Link to={`/details/${filteredBook.id}`}>
+                    <BookCard book={filteredBook} />
+                  </Link>
+                  <button
+                    className="like-btn"
+                    onClick={() => handleLike(filteredBook.id)}
+                  >
+                    <img
+                      width={"50px"}
+                      src={isLike[filteredBook.id] ? likeIcon : unlikeIcon}
+                      alt="heart icon"
+                    />
+                  </button>
+                </li>
+              ))}
+          </ul>
         </div>
+        <button
+          onClick={handleNextFilteredBooks}
+          className="carousel-button next-button"
+        >
+          ›
+        </button>
+      </div>
     </>
   );
 }
--- src/components/booksCard/BookCard.jsx
+++ src/components/booksCard/BookCard.jsx
@@ -1,9 +1,9 @@
-import './BookCard.css'
+import "./BookCard.css";
 
 
 function BookCard({ book }) {
   return (
-    <div className='book-card'>
+    <div className="book-card">
       <h4>{book.title}</h4>
       <img width={200} src={book.image} alt={book.title} />
       <p>{book.author}</p>
